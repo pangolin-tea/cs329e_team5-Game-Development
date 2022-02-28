45696e4f7a541468c3cb6f355c7cf8f1e8330b7f
ananyaprak <ananya1prakash@gmail.com>
2022-02-28 13:37:27 -0600

    new tilemap

--- js/game.js
+++ js/game.js
@@ -1,8 +1,7 @@
-const config = {
+var config = {
     type: Phaser.AUTO, // Which renderer to use
     width: 800, // Canvas width in pixels
     height: 600, // Canvas height in pixels
-    parent: "game-container", // ID of the DOM element to add the canvas to
     scene: {
       preload: preload,
       create: create,
@@ -10,26 +9,43 @@ const config = {
     }
   };
   
-  const game = new Phaser.Game(config);
+  var game = new Phaser.Game(config);
+  var controls;
   
   function preload() {
-    this.load.image("tiles", "../assets/tilesets/tuxmon-sample-32px-extruded.png");
-    this.load.tilemapTiledJSON("map", "../assets/tilemaps/tuxemon-town.json");
+    this.load.image("testTilemap", "assets/TileMapTest.png");
+    this.load.image("tiles", "assets/tilesets/newtileset.png");
+    this.load.tilemapTiledJSON("map", "assets/TileMap1.json");
   }
   
   function create() {
-    const map = this.make.tilemap({ key: "map" });
+    var map = this.make.tilemap({ key: "map" });
   
     // Parameters are the name you gave the tileset in Tiled and then the key of the tileset image in
     // Phaser's cache (i.e. the name you used in preload)
-    const tileset = map.addTilesetImage("tuxmon-sample-32px-extruded", "tiles");
-  
+
+    var tileset = map.addTilesetImage("newtileset", "tiles");
+
     // Parameters: layer name (or index) from Tiled, tileset, x, y
-    const belowLayer = map.createLayer("Below Player", tileset, 0, 0);
-    const worldLayer = map.createLayer("World", tileset, 0, 0);
-    const aboveLayer = map.createLayer("Above Player", tileset, 0, 0);
+    var belowLayer = map.createLayer("Below Player", tileset, 0, 0);
+    var worldLayer = map.createLayer("World", tileset, 0, 0);
+
+    var camera = this.cameras.main;
+
+    var cursors = this.input.keyboard.createCursorKeys();
+    controls = new Phaser.Cameras.Controls.FixedKeyControl({
+        camera: camera,
+        left: cursors.left,
+        right: cursors.right,
+        up: cursors.up,
+        down: cursors.down,
+        speed: 0.5
+    });
+
+    camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+
   }
   
   function update(time, delta) {
-    // Runs once per frame for the duration of the scene
+    controls.update(delta);
   }
