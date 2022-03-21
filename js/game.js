c7536ea87134b979fefcde08936b52c1f85dbe51
pangolin-tea <42625451+pangolin-tea@users.noreply.github.com>
2022-03-21 14:52:47 -0500

    Add files via upload

--- js/game.js
+++ js/game.js
@@ -6,9 +6,102 @@ var enemy1;
 var battle_token;
 var graphics;
 
+var BootScene = new Phaser.Class({
+
+    Extends: Phaser.Scene,
+
+    initialize:
+
+    function BootScene ()
+    {
+        Phaser.Scene.call(this, { key: "BootScene" });
+    },
+	
+    create: function ()
+    {
+        this.scene.start("WorldScene");
+    }
+});
+
+var BattleScene = new Phaser.Class({
+    Extends: Phaser.Scene,
+
+    initialize:
+	
+	function BattleScene ()
+    {
+        Phaser.Scene.call(this, { key: "BattleScene" });
+    },
+    preload: function()
+    {
+        this.load.spritesheet('cat', 'assets/Cat.png', {frameWidth: 32, frameHeight: 32});
+        this.load.spritesheet('bevo', 'assets/bevo.png', {frameWidth: 32, frameHeight: 32});
+        this.load.spritesheet('turt', 'assets/turt.png', {frameWidth: 32, frameHeight:32});
+        this.load.spritesheet('squir', 'assets/squir.png', {frameWidth: 32, frameHeight: 32});
+        this.load.spritesheet('foe', 'assets/a&mfoe.png', {frameWidth: 64, frameHeight: 83});
+        this.load.image('cursor', 'assets/cursor.png');
+    },
+    create: function()
+    {   
+        this.cameras.main.setBackgroundColor('rgba(0, 200, 0, 0.5)');
+		var cat = new PlayerCharacter(this, 100, 150, 'cat', 1, 'Rogue', 80, 8);
+		this.add.existing(cat);
+		var bevo = new PlayerCharacter(this, 100, 250, 'bevo', 1, 'Warrior', 120, 5);
+        this.add.existing(bevo);
+		var turt = new PlayerCharacter(this, 100, 350, 'turt', 1, 'Wizard', 60, 10);
+		this.add.existing(turt);
+		var squir = new PlayerCharacter(this, 100, 450, 'squir', 1, 'Bard', 70, 3);
+		this.add.existing(squir);
+		var foe = new Enemy(this, 600, 300, 'foe', 1, 'Rogue', 100, 6);
+        this.add.existing(foe);
+        
+
+        camera = this.cameras.main.setBackgroundColor('rgba(0, 200, 0, 0.5)');
+		
+		this.heroes = [ cat, bevo, turt, squir];
+		
+		this.enemies = [foe];
+		
+		this.units = this.heroes.concat(this.enemies);
+		
+		this.scene.launch("UIScene");
+		this.index = -1; 
+    },
+	nextTurn: function() {
+        this.index++;
+        // if there are no more units, we start again from the first one
+        if(this.index >= this.units.length) {
+            this.index = 0;
+        }
+        if(this.units[this.index]) {
+            // if its player hero
+            if(this.units[this.index] instanceof PlayerCharacter) {                
+                this.events.emit("PlayerSelect", this.index);
+            } else { // else if its enemy unit
+                // pick random hero
+                var r = Math.floor(Math.random() * this.heroes.length);
+                // call the enemy"s attack function 
+                this.units[this.index].attack(this.heroes[r]);  
+                // add timer for the next turn, so will have smooth gameplay
+                this.time.addEvent({ delay: 3000, callback: this.nextTurn, callbackScope: this });
+            }
+        }
+    },
+    // when the player have selected the enemy to be attacked
+    receivePlayerSelection: function(action, target) {
+        if(action == "attack") {            
+            this.units[this.index].attack(this.enemies[target]);              
+        }
+        // next turn in 3 seconds
+        this.time.addEvent({ delay: 3000, callback: this.nextTurn, callbackScope: this });        
+    }
+});
+
 var Unit = new Phaser.Class({
     Extends: Phaser.GameObjects.Sprite,
+
     initialize:
+
     function Unit(scene, x, y, texture, frame, type, hp, damage) {
         Phaser.GameObjects.Sprite.call(this, scene, x, y, texture, frame)
         this.type = type;
@@ -16,15 +109,21 @@ var Unit = new Phaser.Class({
         this.damage = damage; // default damage                
     },
     attack: function(target) {
-        target.takeDamage(this.damage);      
+        target.takeDamage(this.damage);
+        this.scene.events.emit("Message", this.type + " attacks " + target.type + " for " + this.damage + " damage");
     },
     takeDamage: function(damage) {
-        this.hp -= damage;        
+        this.hp -= damage;
+        if(this.hp <= 0) {
+            this.hp = 0;
+            this.alive = false;
+        }
     }
 });
 
 var Enemy = new Phaser.Class({
     Extends: Unit,
+
     initialize:
     function Enemy(scene, x, y, texture, frame, type, hp, damage) {
         Unit.call(this, scene, x, y, texture, frame, type, hp, damage);
@@ -33,14 +132,34 @@ var Enemy = new Phaser.Class({
 
 var PlayerCharacter = new Phaser.Class({
     Extends: Unit,
+
     initialize:
     function PlayerCharacter(scene, x, y, texture, frame, type, hp, damage) {
         Unit.call(this, scene, x, y, texture, frame, type, hp, damage);
-        // flip the image so I don't have to edit it manually
+        // flip the image so I don"t have to edit it manually
         this.flipX = true;
         
         this.setScale(2);
     }
+});
+
+var MenuItem = new Phaser.Class({
+    Extends: Phaser.GameObjects.Text,
+    
+    initialize:
+            
+    function MenuItem(x, y, text, scene) {
+        Phaser.GameObjects.Text.call(this, scene, x, y, text, { color: "#ffffff", align: "left", fontSize: 15});
+    },
+    
+    select: function() {
+        this.setColor("#f8ff38");
+    },
+    
+    deselect: function() {
+        this.setColor("#ffffff");
+    }
+    
 });
 
 var Menu = new Phaser.Class({
@@ -90,8 +209,8 @@ var Menu = new Phaser.Class({
     },
     confirm: function() {
         // wen the player confirms his slection, do the action
-    },   
-	clear: function() {
+    },
+    clear: function() {
         for(var i = 0; i < this.menuItems.length; i++) {
             this.menuItems[i].destroy();
         }
@@ -99,7 +218,7 @@ var Menu = new Phaser.Class({
         this.menuItemIndex = 0;
     },
     remap: function(units) {
-        this.clear();        
+        this.clear();   	
         for(var i = 0; i < units.length; i++) {
             var unit = units[i];
             this.addMenuItem(unit.type);
@@ -124,10 +243,10 @@ var ActionsMenu = new Phaser.Class({
             
     function ActionsMenu(x, y, scene) {
         Menu.call(this, x, y, scene);   
-        this.addMenuItem('Attack');
+        this.addMenuItem("Attack");
     },
-    confirm: function() {
-        // do something when the player selects an action
+    confirm: function() {      
+        this.scene.events.emit("SelectEnemies");        
     }
     
 });
@@ -141,89 +260,120 @@ var EnemiesMenu = new Phaser.Class({
         Menu.call(this, x, y, scene);        
     },       
     confirm: function() {        
-        // do something when the player selects an enemy
+        this.scene.events.emit("Enemy", this.menuItemIndex);
     }
 });
 
-class UIScene extends Phaser.Scene{
-    constructor(){
-        super({key: 'UIScene'});
-    }
-    create(){
-		this.battleScene = this.scene.get('BattleScene');
+var UIScene = new Phaser.Class({
+
+    Extends: Phaser.Scene,
+
+    initialize:
+
+    function UIScene ()
+    {
+        Phaser.Scene.call(this, { key: "UIScene" });
+    },
+
+    create: function ()
+    {    
         this.graphics = this.add.graphics();
         this.graphics.lineStyle(1, 0xffffff);
         this.graphics.fillStyle(0x031f4c, 1);        
-        this.graphics.strokeRect(102, 500, 190, 100);
-        this.graphics.fillRect(102, 500, 190, 100);
-        this.graphics.strokeRect(295, 500, 190, 100);
-        this.graphics.fillRect(295, 500, 190, 100);
-        this.graphics.strokeRect(488, 500, 190, 100);
-        this.graphics.fillRect(488, 500, 190, 100);
-		this.menus = this.add.container();
+        this.graphics.strokeRect(2, 150, 90, 100);
+        this.graphics.fillRect(2, 150, 90, 100);
+        this.graphics.strokeRect(95, 150, 90, 100);
+        this.graphics.fillRect(95, 150, 90, 100);
+        this.graphics.strokeRect(188, 150, 130, 100);
+        this.graphics.fillRect(188, 150, 130, 100);
+        
+        // basic container to hold all menus
+        this.menus = this.add.container();
                 
-		this.heroesMenu = new HeroesMenu(195, 153, this);           
-		this.actionsMenu = new ActionsMenu(100, 153, this);            
-		this.enemiesMenu = new EnemiesMenu(8, 153, this);   
+        this.heroesMenu = new HeroesMenu(195, 153, this);           
+        this.actionsMenu = new ActionsMenu(100, 153, this);            
+        this.enemiesMenu = new EnemiesMenu(8, 153, this);   
         
-		// the currently selected menu 
-		this.currentMenu = this.actionsMenu;
+        // the currently selected menu 
+        this.currentMenu = this.actionsMenu;
         
-		// add menus to the container
-		this.menus.add(this.heroesMenu);
-		this.menus.add(this.actionsMenu);
-		this.menus.add(this.enemiesMenu);
-	
-		this.remapHeroes();
-		this.remapEnemies();
-    }
-	remapHeroes() {
+        // add menus to the container
+        this.menus.add(this.heroesMenu);
+        this.menus.add(this.actionsMenu);
+        this.menus.add(this.enemiesMenu);
+        
+        this.battleScene = this.scene.get("BattleScene");
+        
+        this.remapHeroes();
+        this.remapEnemies();
+        
+        this.input.keyboard.on("keydown", this.onKeyInput, this);   
+        
+        this.battleScene.events.on("PlayerSelect", this.onPlayerSelect, this);
+        
+        this.events.on("SelectEnemies", this.onSelectEnemies, this);
+        
+        this.events.on("Enemy", this.onEnemy, this);
+        
+        this.message = new Message(this, this.battleScene.events);
+        this.add.existing(this.message);        
+        
+        this.battleScene.nextTurn();                
+    },
+    onEnemy: function(index) {
+        this.heroesMenu.deselect();
+        this.actionsMenu.deselect();
+        this.enemiesMenu.deselect();
+        this.currentMenu = null;
+        this.battleScene.receivePlayerSelection("attack", index);
+    },
+    onPlayerSelect: function(id) {
+        this.heroesMenu.select(id);
+        this.actionsMenu.select(0);
+        this.currentMenu = this.actionsMenu;
+    },
+    onSelectEnemies: function() {
+        this.currentMenu = this.enemiesMenu;
+        this.enemiesMenu.select(0);
+    },
+    remapHeroes: function() {
         var heroes = this.battleScene.heroes;
         this.heroesMenu.remap(heroes);
-    }
-    remapEnemies() {
+    },
+    remapEnemies: function() {
         var enemies = this.battleScene.enemies;
         this.enemiesMenu.remap(enemies);
-    }
-	
-	
-}
+    },
+    onKeyInput: function(event) {
+        if(this.currentMenu) {
+            if(event.code === "ArrowUp") {
+                this.currentMenu.moveSelectionUp();
+            } else if(event.code === "ArrowDown") {
+                this.currentMenu.moveSelectionDown();
+            } else if(event.code === "ArrowRight" || event.code === "Shift") {
 
-class TutorialScene extends Phaser.Scene{
-    constructor(){
-        super({key: 'TutorialScene'});
-    }
-    preload(){
-    }
-    create(){
-        this.add.text(16,104,"Welcome! A&M foes have taken over UT. It's", { fontSize: '30px', color: '#CC5500' })
-        this.add.text(16,134,"your job, along with some surprise helpers,", { fontSize: '30px', color: '#CC5500' })
-        this.add.text(16,164,"to stop them! Use your arrow keys to move", { fontSize: '30px', color: '#CC5500' })
-        this.add.text(16,194,"around, but be careful when you get near an", { fontSize: '28px', color: '#CC5500' })
-        this.add.text(16,224,"enemy...", { fontSize: '28px', color: '#CC5500' })
-
-        this.add.text(16,284,"When you're ready to begin, click your mouse!", { fontSize: '28px', color: '#CC5500' })
-
-        function startGame(){
-            game.scene.start('WorldScene')
+            } else if(event.code === "Space" || event.code === "ArrowLeft") {
+                this.currentMenu.confirm();
+            } 
         }
-        this.input.on('pointerdown', startGame, this);
-    }
-    update(){}
-}
+    },
+});
 
-class WorldScene extends Phaser.Scene {
-    constructor() {
-        super({ key: 'WorldScene' });
-    }
-    preload()
+var WorldScene  = new Phaser.Class({
+	Extends: Phaser.Scene,
+	
+	initialize: 
+    function WorldScene (){
+		Phaser.Scene.call(this, { key: "WorldScene" });
+	},
+    preload: function()
     {
         this.load.image("tiles", "assets/tilesets/newtileset.png");
         this.load.tilemapTiledJSON("map", "assets/TileMap1.json");
         this.load.spritesheet('us', 'assets/utperson.png', { frameWidth: 64, frameHeight: 83 });
         this.load.spritesheet('foe', 'assets/a&mfoe.png', { frameWidth: 64, frameHeight: 83 });
-    }
-    create()
+    },
+    create: function()
     {
         var map = this.make.tilemap({ key: "map" });
 
@@ -235,28 +385,16 @@ class WorldScene extends Phaser.Scene {
     var tileset = map.addTilesetImage("newtileset", "tiles");
 
     // Parameters: layer name (or index) from Tiled, tileset, x, y
-    var belowLayer = map.createStaticLayer("Below Player", tileset, 0, 0);
-    var worldLayer = map.createStaticLayer("World", tileset, 0, 0);
-    var aboveLayer = map.createStaticLayer("Above Player", tileset, 0, 0);
-
-
+    var belowLayer = map.createStaticLayer("Below Player", tileset)
+    var worldLayer = map.createStaticLayer("World", tileset)
 
     worldLayer.setCollisionByExclusion([-1]);
-    // worldLayer.setCollisionByProperty({collides:true}, true, true);
-    belowLayer.setCollisionByExclusion([-1]);
-    // worldLayer.setCollision(3, true, 'World');
-
-    player = this.physics.add.sprite(150, 450, 'us').setSize(24,40).setOffset(19,18);
-    
+   
+    player = this.physics.add.sprite(150, 450, 'us');
     player.setBounce(0.2);
     player.setCollideWorldBounds(true);
 
-    this.physics.add.collider(player, worldLayer);
-    this.physics.add.collider(player, belowLayer);
-
-
-
-    enemy1 = this.physics.add.sprite(200, 325, 'foe').setSize(24,40).setOffset(19,18);
+    enemy1 = this.physics.add.sprite(200, 325, 'foe');
     // enemy1.setBounce(1);
     enemy1.setCollideWorldBounds(true);
     // enemy1.setVelocityX(-180);
@@ -294,9 +432,9 @@ class WorldScene extends Phaser.Scene {
     camera.startFollow(player);
 
     this.physics.add.overlap(player, enemy1, this.onMeetEnemy, false, this)
-    }
+    },
   
-    update(time, delta) {
+    update: function(time, delta) {
     // sprite movement
     
     if (cursors.left.isDown)
@@ -329,49 +467,52 @@ class WorldScene extends Phaser.Scene {
         player.setVelocityY(0);
         player.anims.play('usStraight');
     }
-    }
+    },
 
-    onMeetEnemy() {
+    onMeetEnemy: function() 
+	{
         game.scene.switch('WorldScene', 'BattleScene')
     }
-};
+});
 
-class BattleScene extends Phaser.Scene {
-    constructor(){ 
-        super({ key: 'BattleScene' });
-    }
-    preload()
-    {
-        this.load.spritesheet('cat', 'assets/Cat.png', {frameWidth: 32, frameHeight: 32});
-        this.load.spritesheet('bevo', 'assets/bevo.png', {frameWidth: 32, frameHeight: 32});
-        this.load.spritesheet('turt', 'assets/turt.png', {frameWidth: 32, frameHeight:32});
-        this.load.spritesheet('squir', 'assets/squir.png', {frameWidth: 32, frameHeight: 32});
-        this.load.spritesheet('foe', 'assets/a&mfoe.png', {frameWidth: 64, frameHeight: 83});
-        this.load.image('cursor', 'assets/cursor.png');
-    }
-    create ()
-    {   
-        this.cameras.main.setBackgroundColor('rgba(0, 200, 0, 0.5)');
-		var cat = new PlayerCharacter(this, 100, 150, 'cat', 1, 'Rogue', 80, 8);
-		this.add.existing(cat);
-		var bevo = new PlayerCharacter(this, 100, 250, 'bevo', 1, 'Warrior', 120, 5);
-        this.add.existing(bevo);
-        this.physics.add.sprite(100, 350, 'turt');
-        this.physics.add.sprite(100, 450, 'squir');
-        this.physics.add.sprite(600, 300, 'foe');
-        game.scene.start('UIScene');
+var Message = new Phaser.Class({
 
-        camera = this.cameras.main.setBackgroundColor('rgba(0, 200, 0, 0.5)');
-    }
-    nextTurn ()
-    {
+    Extends: Phaser.GameObjects.Container,
 
+    initialize:
+    function Message(scene, events) {
+        Phaser.GameObjects.Container.call(this, scene, 160, 30);
+        var graphics = this.scene.add.graphics();
+        this.add(graphics);
+        graphics.lineStyle(1, 0xffffff, 0.8);
+        graphics.fillStyle(0x031f4c, 0.3);        
+        graphics.strokeRect(-90, -15, 180, 30);
+        graphics.fillRect(-90, -15, 180, 30);
+        this.text = new Phaser.GameObjects.Text(scene, 0, 0, "", { color: "#ffffff", align: "center", fontSize: 13, wordWrap: { width: 160, useAdvancedWrap: true }});
+        this.add(this.text);
+        this.text.setOrigin(0.5);        
+        events.on("Message", this.showMessage, this);
+        this.visible = false;
+    },
+    showMessage: function(text) {
+        this.text.setText(text);
+        this.visible = true;
+        if(this.hideEvent)
+            this.hideEvent.remove(false);
+        this.hideEvent = this.scene.time.addEvent({ delay: 2000, callback: this.hideMessage, callbackScope: this });
+    },
+    hideMessage: function() {
+        this.hideEvent = null;
+        this.visible = false;
     }
+});
 
-};
+
+
 
 var config = {
     type: Phaser.AUTO, // Which renderer to use
+	parent: "content",
     width: 800, // Canvas width in pixels
     height: 600, // Canvas height in pixels
     physics: {
@@ -381,7 +522,7 @@ var config = {
             debug: false
         }
     },
-    scene: [TutorialScene, WorldScene, BattleScene, UIScene]
+    scene: [BootScene, WorldScene, BattleScene, UIScene]
   };
 
 var game = new Phaser.Game(config);
