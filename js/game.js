4d0264423dbd2d8055f4d5c4db2db03a0c38e98a
ananyaprak <ananya1prakash@gmail.com>
2022-04-18 14:41:38 -0500

    put message class back (oops)

--- js/game.js
+++ js/game.js
@@ -65,6 +65,38 @@ var BootScene = new Phaser.Class({
     }
 });
 
+var Message = new Phaser.Class({
+
+    Extends: Phaser.GameObjects.Container,
+
+    initialize:
+    function Message(scene, events) {
+        Phaser.GameObjects.Container.call(this, scene);
+        var graphics = this.scene.add.graphics();
+        this.add(graphics);
+        graphics.lineStyle(1, 0xffffff, 0.8);
+        graphics.fillStyle(0x031f4c, 0.3);        
+        graphics.strokeRect(-90, -15, 180, 30);
+        graphics.fillRect(-90, -15, 180, 30);
+        this.text = new Phaser.GameObjects.Text(scene, 0, 0, "", { color: "#ffffff", align: "center", fontSize: 15, wordWrap: { width: 180, useAdvancedWrap: true }});
+        this.add(this.text);
+        this.text.setOrigin(0.5);        
+        events.on("Message", this.showMessage, this);
+        this.visible = false;
+    },
+    showMessage: function(text) {
+        this.text.setText(text);
+        this.visible = true;
+        if(this.hideEvent)
+            this.hideEvent.remove(false);
+        this.hideEvent = this.scene.time.addEvent({ delay: 2000, callback: this.hideMessage, callbackScope: this });
+    },
+    hideMessage: function() {
+        this.hideEvent = null;
+        this.visible = false;
+    }
+});
+
 var WorldScene  = new Phaser.Class({
 	Extends: Phaser.Scene,
 	
@@ -168,14 +200,14 @@ var WorldScene  = new Phaser.Class({
         
     onMeetAdvisor: function()
     {
-        this.message("beat enemies", 140, 610);
+        this.message("tutorial", 140, 610);
     },
     
     onMeetEnemy: function() 
 	{  
         player.setVelocityX(0);
         player.setVelocityY(0);
-        var msg = this.add.text(140,610,"What are you doing here!?",{ font: "30px Arial", fill: "#ff0044"});
+        this.message("What are you doing here!?", 475, 710)
         setTimeout(() => { this.scene.switch('BattleScene'); }, 5);
     },
 
