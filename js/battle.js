e5a72bfb5ddbda848a3a4460676251e70ffc2a75
Matthew Stanley <mtthwstanley@gmail.com>
2022-05-02 15:19:53 -0500

    update

--- js/battle.js
+++ js/battle.js
@@ -491,9 +491,7 @@ var UIScene = new Phaser.Class({
                 this.currentMenu.moveSelectionUp();
             } else if(event.code === "ArrowDown" || event.code === "KeyS") {
                 this.currentMenu.moveSelectionDown();
-            } else if(event.code === "ArrowRight" || event.code === "Shift") {
-
-            } else if(event.code === "Space" || event.code === "ArrowLeft") {
+            }  else if(event.code === "Space" || event.code === "ArrowRight") {
                 this.currentMenu.confirm();
             } 
         }
